34363104cf24c506dc0cf0435694b55e9f74840f
giocrds <giovanni.cardoso366@gmail.com>
2024-04-01 21:26:14 -0300

    Algumas alterações

--- main.py
+++ main.py
@@ -2,18 +2,16 @@
 import tkinter as tk
 
 # Definindo as cores
-button_grey = '#ECEFF1'
-button_orange = '#FFAB40'
-font_white = '#FEFFFF'
-display_blue = '#38576b'
+BUTTON_GREY = '#ECEFF1'
+BUTTON_ORANGE = '#FFAB40'
+FONT_WHITE = '#FEFFFF'
+DISPLAY_BLUE = '#38576B'
 
 # Definindo as variáveis
 all_values_math = ''
 all_values_digit = '0'
 last_digit = '0'
-math = ''
 decimals = 0
-isDecimal = False
 
 # Definindo as funções
 
@@ -32,7 +30,10 @@ def evaluate(digit):
     global all_values_math
     global math
     global last_digit
+
     last_ocurrence = 0
+    math = 0
+
     if last_digit in ('+', '-', '*', '/', '%'):
         all_values_math = all_values_math[:-1] + digit
     elif last_digit == '=':
@@ -48,6 +49,7 @@ def evaluate(digit):
     else:
         all_values_math += all_values_digit + digit
         math = eval(all_values_math[:-1])
+
     app_labelmath.config(text=all_values_math)
     all_values_digit = ''
     app_labeldigit.config(text=format_decimal(
@@ -59,11 +61,15 @@ def evaluate(digit):
 
 def updatedigit(digit):
     global all_values_digit
-    global decimals
-    global isDecimal
+    global decimalsisDecimal
     global last_digit
+    global decimals
+
+    isDecimal = 0
+
     if last_digit == '=':
         clear_screen()
+
     if digit in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
         if len(all_values_digit) == 1 and all_values_digit == '0':
             all_values_digit = digit
@@ -79,6 +85,7 @@ def updatedigit(digit):
             app_labeldigit.config(text=format_decimal(
                 float(all_values_digit), decimals) + '.')
             all_values_digit += '.'
+
     last_digit = digit
 
 # Função para alterar a quantidade de decimais
@@ -109,74 +116,82 @@ frame_buttons.grid(row=2, column=0)
 # Criando os labels
 app_labelmath = tk.Label(frame_displaymath,
                          width=37, height=2, padx=7, relief='flat', anchor='e',
-                         justify='right', font=('Ivy 8'), bg=display_blue,
-                         fg=font_white)
+                         justify='right', font=('Ivy 8'), bg=DISPLAY_BLUE,
+                         fg=FONT_WHITE)
 app_labelmath.place(x=0, y=0)
 
 app_labeldigit = tk.Label(frame_displaydigit, text='0',
                           width=16, height=2, padx=7, relief='flat',
                           anchor='e', justify='right', font=('Ivy 18'),
-                          bg=display_blue, fg=font_white)
+                          bg=DISPLAY_BLUE, fg=FONT_WHITE)
 app_labeldigit.place(x=0, y=0)
 
 # Criando botões
 
 # Botão C (Clear), % (Porcentagem) e / (Divisão)
 button_c = tk.Button(frame_buttons, text="C", width=11, height=2,
-                     bg=button_grey, font=('Ivy 13 bold'), relief='raised',
+                     bg=BUTTON_GREY, font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: clear_screen())
 button_c.place(x=0, y=0)
+
 button_percentage = tk.Button(frame_buttons, text='%',
-                              width=5, height=2, bg=button_grey,
+                              width=5, height=2, bg=BUTTON_GREY,
                               font=('Ivy 13 bold'), relief='raised',
                               overrelief='ridge',
                               command=lambda: evaluate('%'))
 button_percentage.place(x=118, y=0)
+
 button_d = tk.Button(frame_buttons, text='/', width=5,
-                     height=2, bg=button_orange, fg=font_white,
+                     height=2, bg=BUTTON_ORANGE, fg=FONT_WHITE,
                      font=('Ivy 13 bold'),  relief='raised',
                      overrelief='ridge', command=lambda: evaluate('/'))
 button_d.place(x=177, y=0)
 
 # Botão 7, 8, 9 e * (Multiplicação)
 button_7 = tk.Button(frame_buttons, text='7', width=5, height=2,
-                     bg=button_grey, font=('Ivy 13 bold'), relief='raised',
+                     bg=BUTTON_GREY, font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('7'))
 button_7.place(x=0, y=52)
+
 button_8 = tk.Button(frame_buttons, text='8',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('8'))
 button_8.place(x=59, y=52)
+
 button_9 = tk.Button(frame_buttons, text='9',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('9'))
 button_9.place(x=118, y=52)
+
 button_multiply = tk.Button(frame_buttons, text='*', width=5,
-                            height=2, bg=button_orange, fg=font_white,
+                            height=2, bg=BUTTON_ORANGE, fg=FONT_WHITE,
                             font=('Ivy 13 bold'),  relief='raised',
                             overrelief='ridge', command=lambda: evaluate('*'))
 button_multiply.place(x=177, y=52)
 
 # Botão 4, 5, 6 e - (Subtração)
 button_4 = tk.Button(frame_buttons, text='4',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('4'))
 button_4.place(x=0, y=104)
+
 button_5 = tk.Button(frame_buttons, text='5',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('5'))
 button_5.place(x=59, y=104)
+
 button_6 = tk.Button(frame_buttons, text='6',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('6'))
 button_6.place(x=118, y=104)
+
 button_subtract = tk.Button(frame_buttons, text='-', width=5,
-                            height=2, bg=button_orange, fg=font_white,
+                            height=2, bg=BUTTON_ORANGE, fg=FONT_WHITE,
                             font=('Ivy 13 bold'),  relief='raised',
                             overrelief='ridge',
                             command=lambda: evaluate('-'))
@@ -184,38 +199,43 @@ button_subtract.place(x=177, y=104)
 
 # Botão 1, 2, 3 e + (Soma)
 button_1 = tk.Button(frame_buttons, text='1',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('1'))
 button_1.place(x=0, y=156)
+
 button_2 = tk.Button(frame_buttons, text='2',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('2'))
 button_2.place(x=59, y=156)
+
 button_3 = tk.Button(frame_buttons, text='3',
-                     width=5, height=2, bg=button_grey,
+                     width=5, height=2, bg=BUTTON_GREY,
                      font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('3'))
 button_3.place(x=118, y=156)
+
 button_sum = tk.Button(frame_buttons, text='+', width=5,
-                       height=2, bg=button_orange, fg=font_white,
+                       height=2, bg=BUTTON_ORANGE, fg=FONT_WHITE,
                        font=('Ivy 13 bold'),  relief='raised',
                        overrelief='ridge', command=lambda: evaluate('+'))
 button_sum.place(x=177, y=156)
 
 # Botão 0, . (Separação) e = (Igual)
 button_0 = tk.Button(frame_buttons, text="0", width=11, height=2,
-                     bg=button_grey, font=('Ivy 13 bold'), relief='raised',
+                     bg=BUTTON_GREY, font=('Ivy 13 bold'), relief='raised',
                      overrelief='ridge', command=lambda: updatedigit('0'))
 button_0.place(x=0, y=208)
+
 button_point = tk.Button(frame_buttons, text='.',
-                         width=5, height=2, bg=button_grey,
+                         width=5, height=2, bg=BUTTON_GREY,
                          font=('Ivy 13 bold'), relief='raised',
                          overrelief='ridge', command=lambda: updatedigit('.'))
 button_point.place(x=118, y=208)
+
 button_equals = tk.Button(frame_buttons, text='=', width=5,
-                          height=2, bg=button_orange, fg=font_white,
+                          height=2, bg=BUTTON_ORANGE, fg=FONT_WHITE,
                           font=('Ivy 13 bold'),  relief='raised',
                           overrelief='ridge', command=lambda: evaluate('='))
 button_equals.place(x=177, y=208)
